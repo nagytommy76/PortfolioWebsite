6117a512f6df2ef2e969e0b2451fc911aaa69b6f
nagytommy76 <nagytommy76@gmail.com>
2023-08-21 20:10:03 +0200

    Created a projects page

---
+++ app/projects/page.tsx
@@ -0,0 +1,9 @@
+const Projects = () => {
+   return (
+      <section id='projects' className='h-screen bg-main-bg text-white'>
+         <h1>Projektek</h1>
+      </section>
+   )
+}
+
+export default Projects
