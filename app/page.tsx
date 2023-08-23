ed1a009e0bd20477e8dbb971f234e1002b8fe7d7
nagytommy76 <nagytommy76@gmail.com>
2023-08-23 20:21:19 +0200

    Added the About & Projects component

--- app/page.tsx
+++ app/page.tsx
@@ -1,10 +1,13 @@
 import Navbar from './components/navbar'
+import About from './components/about'
+import Projects from './projects/page'
 
 export default function Home() {
    return (
       <main className='h-screen'>
          <Navbar />
-         <h1 className='text-blue-400 text-5xl'>Nagy Tamás</h1>
+         <About />
+         <Projects />
       </main>
    )
 }
