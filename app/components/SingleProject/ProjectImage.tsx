3a1118b46e3b6485bd7b49250117bbf515c2f3c5
nagytommy76 <nagytommy76@gmail.com>
2023-09-02 17:10:53 +0200

    Created an image slider using materialUI's modal

--- app/components/SingleProject/ProjectImage.tsx
+++ app/components/SingleProject/ProjectImage.tsx
@@ -2,15 +2,90 @@
 import { useState } from 'react'
 import Image from 'next/image'
 
-const ProjectImage: React.FC<{ imageLink: string; alt: string }> = ({ imageLink, alt }) => {
-   const [openImage, setOpenImage] = useState(false)
+import Modal from '@mui/material/Modal'
+import CloseIcon from '@mui/icons-material/Close'
+import ArrowForwardIosIcon from '@mui/icons-material/ArrowForwardIos'
+import ArrowBackIosNewIcon from '@mui/icons-material/ArrowBackIosNew'
+
+const ProjectImage: React.FC<{ folder: string; imageNames: string[]; alt: string }> = ({
+   folder,
+   imageNames,
+   alt,
+}) => {
+   const [openImage, setOpenImage] = useState<boolean>(false)
+   const [currentPicIndex, setCurrentPicIndex] = useState<number>(0)
+
    const handleOpen = () => {
       setOpenImage(true)
    }
+
+   const handleClose = () => {
+      setOpenImage(false)
+   }
+
+   const nextImage = () => {
+      if (imageNames.length > 1) {
+         setCurrentPicIndex(currentPicIndex === imageNames.length - 1 ? 0 : currentPicIndex + 1)
+      }
+   }
+   const previousImage = () => {
+      if (imageNames.length > 1) {
+         setCurrentPicIndex(currentPicIndex === 0 ? imageNames.length - 1 : currentPicIndex - 1)
+      }
+   }
+
    return (
-      <div>
-         <Image onClick={handleOpen} className='' src={imageLink} alt={alt} width={700} height={600} />
-      </div>
+      <>
+         <Modal
+            open={openImage}
+            onClose={setOpenImage}
+            aria-labelledby='child-modal-title'
+            aria-describedby='child-modal-description'
+         >
+            <section>
+               <CloseIcon
+                  onClick={handleClose}
+                  className='absolute right-5 top-5 cursor-pointer text-amber-600'
+                  fontSize='large'
+                  sx={{ fontSize: 55 }}
+               />
+               <ArrowBackIosNewIcon
+                  onClick={previousImage}
+                  className='absolute left-5 top-[50%] cursor-pointer text-amber-600'
+                  fontSize='large'
+                  sx={{ fontSize: 55 }}
+               />
+               <ArrowForwardIosIcon
+                  onClick={nextImage}
+                  className='absolute right-5 top-[50%] cursor-pointer text-amber-600'
+                  sx={{ fontSize: 55 }}
+               />
+               <div className='w-screen h-screen flex items-center justify-center'>
+                  <Image
+                     sizes='(max-width: 400px) 400px, (max-width: 1200px) 1200px, 100vw'
+                     className=''
+                     src={`https://res.cloudinary.com/domsczfqu/image/upload/v1660310038/nagytamas93/${folder}/${imageNames[currentPicIndex]}`}
+                     alt={alt}
+                     width={1920}
+                     height={1080}
+                  />
+               </div>
+               <p className='text-4xl text-white absolute top-10 left-[50%]'>
+                  {currentPicIndex + 1}/{imageNames.length}
+               </p>
+            </section>
+         </Modal>
+         <div className=''>
+            <Image
+               onClick={handleOpen}
+               className=''
+               src={`https://res.cloudinary.com/domsczfqu/image/upload/v1660310038/nagytamas93/${folder}/${imageNames[0]}`}
+               alt={alt}
+               width={700}
+               height={600}
+            />
+         </div>
+      </>
    )
 }
 
