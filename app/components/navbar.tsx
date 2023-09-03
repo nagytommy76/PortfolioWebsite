daec41d2a7474422de8871a8c47ddf3fbcfc6d2f
nagytommy76 <nagytommy76@gmail.com>
2023-09-03 14:41:05 +0200

    Using the Contact icons component

--- app/components/navbar.tsx
+++ app/components/navbar.tsx
@@ -1,33 +1,92 @@
-import Image from 'next/image'
-import Link from 'next/link'
+'use client'
+import { useState, useEffect, useCallback } from 'react'
 
-import GithubIcon from '@/icons/github.svg'
-import Linkedincon from '@/icons/linkedin.svg'
+import CloseIcon from '@mui/icons-material/Close'
+import MenuIcon from '@mui/icons-material/Menu'
 
+import Collapse from '@mui/material/Collapse'
+
+import ContactIcons from '@/components/includes/ContactIcons'
 import HoverLink from '../../src/hoverLink'
 
-const navbar = () => {
+const Navbar = () => {
+   const [isOpen, setIsOpen] = useState<boolean>(true)
+   const [isMobile, setIsMobile] = useState<boolean>(false)
+
+   const handleCloseNavbar = useCallback(() => {
+      isMobile && setIsOpen(false)
+   }, [isMobile])
+   const handleOpenNavbar = () => setIsOpen(true)
+
+   const handleWindowSizeChange = useCallback(() => {
+      if (window.innerWidth <= 639) {
+         setIsMobile(true)
+         handleCloseNavbar()
+      } else {
+         handleOpenNavbar()
+         setIsMobile(false)
+      }
+   }, [handleCloseNavbar])
+
+   useEffect(() => {
+      handleWindowSizeChange()
+      window.addEventListener('resize', handleWindowSizeChange)
+      window.addEventListener('load', handleWindowSizeChange)
+      return () => {
+         window.removeEventListener('resize', handleWindowSizeChange)
+         window.removeEventListener('load', handleWindowSizeChange)
+      }
+   }, [handleWindowSizeChange])
+
    return (
-      <header className='bg-transparent text-white z-50'>
-         <nav className='h-20 w-full fixed top-0 flex flex-wrap flex-row content-center justify-around z-50'>
-            <section className='w-1/3 flex flex-wrap flex-row content-center justify-between text-xl z-50'>
-               <HoverLink href='#home' linkText='Logó' />
-               <HoverLink href='#about' linkText='Rólam' />
-               <HoverLink href='#projects' linkText='Projektek' />
-               <HoverLink href='/' linkText='Skillek' />
-               <HoverLink href='/' linkText='Cert' />
-            </section>
-            <section className='w-24 flex flex-wrap flex-row justify-around content-center'>
-               <Link className='self-center' target='_blank' href='https://www.linkedin.com/in/tamasnagy93'>
-                  <Image className='' src={Linkedincon} alt='Linkedincon' width={40} height={40} />
-               </Link>
-               <Link className='self-center' target='_blank' href='https://github.com/nagytommy76'>
-                  <Image src={GithubIcon} alt='Githubicon' width={40} height={40} />
-               </Link>
-            </section>
-         </nav>
-      </header>
+      <>
+         {isMobile && (
+            <MenuIcon
+               onClick={handleOpenNavbar}
+               fontSize='large'
+               className={`z-[50] fixed right-5 top-5 text-amber-500 ${isMobile ? 'flex' : 'md:hidden'}`}
+            />
+         )}
+         {/* <Collapse in={isMobile ? isOpen : true}> */}
+         <Collapse in={isOpen}>
+            <nav
+               className='
+               text-white
+               bg-zinc-800 opacity-95 shadow-md h-[75px] w-full fixed top-0
+               flex flex-wrap flex-row content-center justify-around z-50
+               sm:h-screen sm:opacity-100 sm:flex-col sm:justify-center
+               '
+            >
+               {isMobile && (
+                  <CloseIcon
+                     onClick={handleCloseNavbar}
+                     fontSize='large'
+                     className={`fixed right-5 top-5 text-amber-500 ${isMobile ? 'flex' : 'md:hidden'}`}
+                  />
+               )}
+               <section
+                  className='
+                  w-1/3 flex flex-wrap flex-row content-center justify-between text-xl z-50
+                  sm:w-screen sm:flex-col sm:h-[40%] sm:text-center
+                  '
+               >
+                  <HoverLink onClick={handleCloseNavbar} href='#home' linkText='Logó' />
+                  <HoverLink onClick={handleCloseNavbar} href='#about' linkText='Rólam' />
+                  <HoverLink onClick={handleCloseNavbar} href='#projects' linkText='Projektek' />
+                  <HoverLink onClick={handleCloseNavbar} href='/' linkText='Skillek' />
+                  <HoverLink onClick={handleCloseNavbar} href='/' linkText='Cert' />
+               </section>
+               <section
+                  className='
+                  w-36 flex flex-wrap flex-row justify-around content-center items-center
+                   sm:justify-around sm:mx-auto sm:mt-11'
+               >
+                  <ContactIcons />
+               </section>
+            </nav>
+         </Collapse>
+      </>
    )
 }
 
-export default navbar
+export default Navbar
