7d2c91503e21162f653935c3fa55bc9c6b990d1f
nagytommy76 <nagytommy76@gmail.com>
2023-09-03 11:50:18 +0200

    Optimized for mobile, smaller text and tech icons

--- app/components/about.tsx
+++ app/components/about.tsx
@@ -7,18 +7,26 @@ import MainText from './includes/MainText'
 
 const about = () => {
    return (
-      <section id='about' className='pt-20 h-screen text-slate-50 bg-hero bg-no-repeat bg-cover bg-center'>
+      <section
+         id='about'
+         className='pt-20 md:h-screen text-slate-50 bg-hero bg-no-repeat bg-cover bg-center sm:min-h-screen sm:pt-6'
+      >
          <MainText text='Rólam' />
-         <div className='max-w-6xl mx-auto h-4/5 flex flex-row flex-wrap content-center items-center justify-center'>
-            <div className='w-2/4 flex flex-wrap justify-center pr-10'>
+         <div
+            className='
+            max-w-6xl mx-auto h-4/5 flex flex-row flex-wrap content-center items-center justify-center
+            sm:h-full sm:w-full
+            '
+         >
+            <div className='w-2/4 flex flex-wrap justify-center pr-10 sm:pr-0 sm:content-center sm:w-[85%]'>
                <Image
-                  className='rounded-full bg-cover w-64 h-64 mb-10'
+                  className='rounded-full object-cover object-top w-64 h-64 mb-10 sm:w-44 sm:h-44 sm:mt-10'
                   src={MeImage}
                   height={250}
                   width={250}
                   alt='Nagy Tamás Képe'
                />
-               <p className='text-xl leading-8 text-left mb-5'>
+               <p className='text-xl leading-8 text-left mb-5 sm:text-sm sm:mb-10 sm:leading-6'>
                   Jelenlegi tudásom nagy részét autodidakta módon, Udemy kurzusok segítségével, valamint hobby
                   projectek elkészítésével szereztem. Körülbelül 2 éve foglalkozom rendszeresen programozással
                   szabadidőmben. Kezdőként Forntend (React) területen szeretnék elhelyezkedni, illetve
--- app/components/includes/TechIcons.tsx
+++ app/components/includes/TechIcons.tsx
@@ -5,17 +5,17 @@ import HTML5 from '@/icons/html5.svg'
 import JavaScript from '@/icons/javascript.svg'
 import TypeScript from '@/icons/typescript.svg'
 import ReactIcon from '@/icons/react.svg'
-import NextJs from '@/icons/nextjs.svg'
 import NodeJs from '@/icons/nodejs.svg'
 import ExpressJs from '@/icons/express.svg'
 import MongoDB from '@/icons/mongodb.svg'
 import SASS from '@/icons/sass.svg'
 import GIT from '@/icons/git.svg'
 import Docker from '@/icons/docker.svg'
+import Next2 from '@/icons/next.svg'
 
 const TechIcons = () => {
    return (
-      <div className='w-2/4 h-3/5 pl-10 flex flex-wrap flex-row justify-between'>
+      <div className='w-2/4 h-3/5 pl-10 flex flex-wrap flex-row justify-between sm:w-[95%] sm:pl-0 sm:mt-9'>
          <div className='flex flex-wrap flex-col justify-evenly'>
             <StackIcon alt='CSS3' displayText='CSS3' src={CSS3} />
             <StackIcon alt='HTML5' displayText='HTML5' src={HTML5} />
@@ -24,7 +24,7 @@ const TechIcons = () => {
          </div>
          <div className='flex flex-wrap flex-col justify-evenly'>
             <StackIcon alt='React' displayText='React' src={ReactIcon} />
-            <StackIcon alt='NextJs' displayText='Next.js' src={NextJs} />
+            <StackIcon alt='NextJs' displayText='Next.js' src={Next2} />
             <StackIcon alt='NodeJs' displayText='Node.js' src={NodeJs} />
             <StackIcon alt='ExpressJs' displayText='Express.js' src={ExpressJs} />
          </div>
