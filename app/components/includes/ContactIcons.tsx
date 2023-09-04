f310ba4343c0161147883a3308c0814bb197b5b8
nagytommy76 <nagytommy76@gmail.com>
2023-09-04 08:28:39 +0200

    Added a CV link icon with tooltip

--- app/components/includes/ContactIcons.tsx
+++ app/components/includes/ContactIcons.tsx
@@ -4,6 +4,7 @@ import Link from 'next/link'
 import EmailIcon from '@mui/icons-material/Email'
 import GitHubIcon from '@mui/icons-material/GitHub'
 import LinkedInIcon from '@mui/icons-material/LinkedIn'
+import ContactPageIcon from '@mui/icons-material/ContactPage'
 
 import Tooltip from '@mui/material/Tooltip'
 
@@ -38,6 +39,15 @@ const ContactIcons = () => {
                <EmailIcon fontSize='large' />
             </Tooltip>
          </p>
+         <Link
+            href={'https://drive.google.com/drive/folders/1-kAtpIRgO-xTEtikH-HyR4HCJm_HuhFu?usp=sharing'}
+            className='cursor-pointer transition hover:-translate-y-1'
+            target='_blank'
+         >
+            <Tooltip title={'Önéletrajz'} placement='bottom' arrow>
+               <ContactPageIcon fontSize='large' />
+            </Tooltip>
+         </Link>
       </>
    )
 }
