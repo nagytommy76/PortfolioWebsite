0d6a77a7e3d9bceedbab276f836cc4be529ca751
nagytommy76 <nagytommy76@gmail.com>
2023-09-04 19:47:44 +0200

    Added a min-max width & set the duration to 380ms

--- app/components/SingleProject/SingleProject.tsx
+++ app/components/SingleProject/SingleProject.tsx
@@ -24,9 +24,9 @@ const SingleProject: React.FC<{
 }) => {
    return (
       <div
-         className={`mt-24 w-full flex flex-wrap ${
+         className={`mt-32 w-full flex flex-wrap ${
             isReverse ? 'flex-row-reverse' : ''
-         } justify-between sm:flex-col`}
+         } justify-between sm:flex-col sm:mt-16`}
       >
          <div className='relative w-auto cursor-pointer transition-all hover:scale-[98%]'>
             {ProjectImageComponent}
@@ -41,8 +41,8 @@ const SingleProject: React.FC<{
             )}
          </div>
          <div className={`w-1/3 flex flex-col justify-between sm:w-[95%] sm:mt-3 sm:mx-auto `}>
-            <h1 className='text-5xl tracking-wide sm:text-3xl sm:mb-3'>{headText}</h1>
-            <p className='font-semibold sm:text-sm sm:mb-4 sm:font-normal sm:text-justify'>{description}</p>
+            <h1 className='text-5xl font-semibold tracking-normal sm:text-3xl sm:mb-3'>{headText}</h1>
+            <p className='font-normal sm:text-sm sm:mb-4 sm:font-normal sm:text-justify'>{description}</p>
             <span className='h-24 flex flex-col justify-between'>
                {!isUnderDev ? (
                   <LinkButton displayText='Élő verzió' href={projectHref} />
