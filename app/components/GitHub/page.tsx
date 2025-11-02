68befb71c74f3c7b7bc2dbc8de054940200295b8
nagytommy76 <nagytommy76@gmail.com>
2025-11-02 10:59:50 +0100

    Add new img
    Add commit img from 2020-2025 using an array & add GitHub stats as img

--- app/components/GitHub/page.tsx
+++ app/components/GitHub/page.tsx
@@ -1,59 +1,55 @@
+/* eslint-disable @next/next/no-img-element */
 import MainText from '../includes/MainText'
 import ProjectImage from '../SingleProject/ProjectImage'
+import LinkButton from '@/src/LinkButton/LinkButton'
+
+import GitHubIcon from '@/icons/github.svg'
+
+const githubYears = [2025, 2024, 2023, 2022, 2021, 2020]
 
 export default function page() {
    return (
-      <section id='github' className='min-h-fit max-w-6xl mx-auto mb-16 pt-20 text-white'>
+      <section id='github' className='min-h-fit max-w-7xl mx-auto mb-16 pt-20 text-white'>
          <MainText text='GitHub' />
          <p className='text-base leading-8 text-center mt-10 sm:text-sm sm:mb-10 sm:leading-6'>
             Since mid-2020, I have been continuously improving myself by creating various pet projects.
          </p>
-         <div className='mt-4 w-full flex flex-wrap justify-evenly items-center gap-4'>
-            <div className='cursor-pointer'>
+         <div className='mt-4 w-full flex flex-wrap justify-evenly gap-2'>
+            {githubYears.map((year) => (
                <ProjectImage
-                  alt='GitHub 2020'
+                  key={year}
+                  alt={`GitHub ${year}`}
                   folder='GitHub'
-                  imageNames={['uhxv8vwaid0lre1twx8e']}
-                  imageWidth={810}
+                  imageNames={[`${year}_github`]}
+                  imageWidth={610}
                   imageHeight={190}
                />
-            </div>
-            <div className='cursor-pointer'>
-               <ProjectImage
-                  alt='GitHub 2021'
-                  folder='GitHub'
-                  imageNames={['vzzixypzte89rvw6wqh2']}
-                  imageWidth={810}
-                  imageHeight={190}
-               />
-            </div>
-            <div className='cursor-pointer'>
-               <ProjectImage
-                  alt='GitHub 2022'
-                  folder='GitHub'
-                  imageNames={['v3z8x1qihzlcckphkn7o']}
-                  imageWidth={810}
-                  imageHeight={190}
-               />
-            </div>
-            <div className='cursor-pointer'>
-               <ProjectImage
-                  alt='GitHub 2023'
-                  folder='GitHub'
-                  imageNames={['l5uuiamt6r1t7qmi6xpl']}
-                  imageWidth={810}
-                  imageHeight={190}
-               />
-            </div>
-            <div className='cursor-pointer'>
-               <ProjectImage
-                  alt='GitHub 2024'
-                  folder='GitHub'
-                  imageNames={['m0ohhj3epwtbfmkuq1ms']}
-                  imageWidth={810}
-                  imageHeight={190}
-               />
-            </div>
+            ))}
+         </div>
+         <div className='flex items-center justify-center justify-items-center mt-7 gap-2 sm:flex-col'>
+            <img
+               width='600px'
+               src='https://github-readme-stats.vercel.app/api?username=nagytommy76&show_icons=true&theme=gruvbox&hide_border=true'
+               alt='nagy-tamas'
+            />
+            <img
+               width='600px'
+               src='https://github-readme-streak-stats.herokuapp.com/?user=nagytommy76&include_all_commits=true&show_icons=true&count_private=true&locale=en&theme=gruvbox&hide_border=true&border_radius=5'
+               alt='nagy-tamas'
+            />
+            <img
+               width='400px'
+               src='https://github-readme-stats.vercel.app/api/top-langs/?username=nagytommy76&layout=compact&langs_count=8&theme=gruvbox&hide_border=true&border_radius=5'
+               alt='nagy-tamas'
+            />
+         </div>
+         <div className='w-full flex items-center justify-center mt-10'>
+            <LinkButton
+               className='min-w-[400px]'
+               displayText='Vitsit My GitHub Page'
+               href='https://github.com/nagytommy76'
+               iconElement={GitHubIcon}
+            />
          </div>
       </section>
    )
